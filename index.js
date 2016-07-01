7f6728ffafaf6923da2c42db349bbc9643b721e7
Per Jansson <per.r.jansson@gmail.com>
2016-07-01 22:53:49 +0200

    minor refactoring

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', (request, response) => { response.send('It works!'); });
+app.get('/', (request, response) => { response.send('It works!!!'); });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+app.listen(app.get('port'), console.log('Node app is running on port... ', app.get('port')));
