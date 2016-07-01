c630a3bda1f2e25d993f20eb04b0ef94d13427af
Per Jansson <per.r.jansson@gmail.com>
2016-07-01 22:51:12 +0200

    arrow function

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send('It works :)');
-});
+app.get('/', (request, response) => { response.send('It works!'); });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
